624a49ff5548abaaa30b73cf4b8d665b46e6d371
Thomas ZHAO <78357406+tzhao-symphony@users.noreply.github.com>
2025-04-10 18:27:44 +0200

    update config

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -4,8 +4,8 @@ import * as process from "node:process";
 
 export const config = {
     web: {
-        port: process.env.CSP_LOGGER_PORT || 8080,
-        address: process.env.CSP_LOGGER_HOST || '0.0.0.0',
+        port: process.env.LOGGER_PORT || 8080,
+        address: process.env.LOGGER_HOST || '0.0.0.0',
         // https: {
         //     key: process.env.CSP_LOGGER_KEY || `${join(import.meta.dirname, '../../certs/csp-logger.key')}`,
         //     cert: process.env.CSP_LOGGER_CERT || `${join(import.meta.dirname, '../../certs/csp-logger.crt')}`
